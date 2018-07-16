ac6c7e7bd3cb0554468f0f37455cab807dc1a454
Bane Sullivan <banesullivan@gmail.com>
2018-07-16 03:56:21 -0600

    Implemented VolumeElement VTK conversion

--- omf/volume.py
+++ omf/volume.py
@@ -62,6 +62,10 @@ class VolumeGridGeometry(ProjectElementGeometry):
         """Number of cells"""
         return len(self.tensor_u) * len(self.tensor_v) * len(self.tensor_w)
 
+    @property
+    def shape(self):
+        return ( len(self.tensor_u), len(self.tensor_v), len(self.tensor_w))
+
     @properties.validator
     def _validate_mesh(self):
         """Check if mesh content is built correctly"""
@@ -79,14 +83,54 @@ class VolumeGridGeometry(ProjectElementGeometry):
         import vtk
         from vtk.util import numpy_support as nps
 
-        # if axis orientations are standard then use a vtkRectilinearGrid
-        #- otherwise use a vtkStructuredGrid
+        self._validate_mesh()
+
+        ox, oy, oz = self.origin
+
+        def checkOrientation():
+            if np.allclose(self.axis_u, (1,0,0)) and np.allclose(self.axis_v, (0,1,0)) and np.allclose(self.axis_w, (0,0,1)):
+                return True
+            return False
+
+        def rotationMatrix():
+            # TODO: is this correct?
+            return np.array([self.axis_u, self.axis_v, self.axis_w])
+
+        # Make coordinates along each axis
+        x = ox + np.cumsum(self.tensor_u)
+        x = np.insert(x, 0, ox)
+        y = oy + np.cumsum(self.tensor_v)
+        y = np.insert(y, 0, oy)
+        z = oz + np.cumsum(self.tensor_w)
+        z = np.insert(z, 0, oz)
+
+        # If axis orientations are standard then use a vtkRectilinearGrid
+        if checkOrientation():
+            output = vtk.vtkRectilinearGrid()
+            output.SetDimensions(len(x), len(y), len(z)) # note this subtracts 1
+            output.SetXCoordinates(nps.numpy_to_vtk(num_array=x))
+            output.SetYCoordinates(nps.numpy_to_vtk(num_array=y))
+            output.SetZCoordinates(nps.numpy_to_vtk(num_array=z))
+            return output
+
+        # Otherwise use a vtkStructuredGrid
         output = vtk.vtkStructuredGrid()
+        output.SetDimensions(len(x), len(y), len(z)) # note this subtracts 1
 
-        # TODO: build!
         # Build out all nodes in the mesh
+        xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
+        points = np.stack( (xx.flatten(), yy.flatten(), zz.flatten()) ).T
 
-        # Add to output
+        # Rotate the points based on the axis orientations
+        rmtx = rotationMatrix()
+        points = points.dot(rmtx)
+
+        # Convert points to vtk object
+        pts = vtk.vtkPoints()
+        for r in points:
+            pts.InsertNextPoint(r[0], r[1], r[2])
+        # Now build the output
+        output.SetPoints(pts)
 
         return output
 
@@ -105,5 +149,14 @@ class VolumeElement(ProjectElement):
 
     def toVTK(self):
         """Convert the volume element to a VTK data object."""
+        from vtk.util import numpy_support as nps
         output = self.geometry.toVTK()
+        shp = self.geometry.shape
+        # Add data to output
+        for data in self.data:
+            arr = data.array.array
+            arr = np.reshape(arr, shp).flatten(order='F')
+            c = nps.numpy_to_vtk(num_array=arr, deep=True)
+            c.SetName(data.name)
+            output.GetCellData().AddArray(c)
         return output
