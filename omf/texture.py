5030d4ce7f07c6abc12389cf41581354b140295e
Franklin Koch <franklin.koch@seequent.com>
2021-07-06 12:07:17 -0600

    GH-36 Serialize images the same way as arrays

--- omf/texture.py
+++ omf/texture.py
@@ -4,12 +4,67 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import io
+import uuid
+
 import properties
 
-from .base import ContentModel
+from .base import BaseModel, ContentModel
 from .data import ArrayInstanceProperty
-from .serializers import png_serializer, png_deserializer
 
+
+class Image(BaseModel):
+    """Class for setting PNG image"""
+    schema_type = 'org.omf.v2.image.png'
+
+    image = properties.ImagePNG(
+        'PNG image file',
+        serializer=lambda *args, **kwargs: None,
+        deserializer=lambda *args, **kwargs: None,
+    )
+
+    def __init__(self, image=None, **kwargs):
+        super(Image, self).__init__(**kwargs)
+        if image is not None:
+            self.image = image
+
+    @properties.StringChoice(
+        'Image data type string', choices=['png']
+    )
+    def datatype(self):
+        """Image type descriptor, currently only PNGs are supported"""
+        return 'png'
+
+    @properties.Integer('Size of image in bytes')
+    def size(self):
+        """Total size of the array in bits"""
+        if self.image is None:
+            return None
+        size = self.image.seek(0, 2)
+        self.image.seek(0)
+        return size
+
+    def serialize(self, include_class=True, save_dynamic=False, **kwargs):
+        output = super(Image, self).serialize(include_class=include_class, save_dynamic=True, **kwargs)
+        image_uid = str(uuid.uuid4())
+        binary_dict = kwargs.get('binary_dict', None)
+        if binary_dict is not None:
+            self.image.seek(0)
+            binary_dict.update({image_uid: self.image.read()})
+        output.update({'image': image_uid})
+        return output
+
+    @classmethod
+    def deserialize(cls, value, trusted=False, strict=False,
+                    assert_valid=False, **kwargs):
+        binary_dict = kwargs.get('binary_dict', {})
+        if not isinstance(value, dict):
+            pass
+        elif 'image' not in value:
+            pass
+        elif value['image'] in binary_dict:
+            return cls(io.BytesIO(binary_dict[value['image']]))
+        return cls()
 
 class ProjectedTexture(ContentModel):
     """Contains an image that can be projected onto a point set or surface"""
@@ -27,20 +82,18 @@ class ProjectedTexture(ContentModel):
         'Vector corresponding to the image y-axis',
         default='Y',
     )
-    image = properties.ImagePNG(
+    image = properties.Instance(
         'PNG image file',
-        serializer=png_serializer,
-        deserializer=png_deserializer,
+        Image,
     )
 
 
 class UVMappedTexture(ContentModel):
     """Contains an image that is UV mapped to a geometry"""
 
-    image = properties.ImagePNG(
+    image = properties.Instance(
         'PNG image file',
-        serializer=png_serializer,
-        deserializer=png_deserializer,
+        Image,
     )
     uv_coordinates = ArrayInstanceProperty(
         'Normalized UV coordinates mapping the image to element vertices; '
@@ -69,12 +122,12 @@ class HasTexturesMixin(properties.HasProperties):
         for i, tex in enumerate(self.textures):
             if isinstance(tex, ProjectedTexture):
                 continue
-            if len(tex.uv_coordinates) != self.num_nodes:
+            if len(tex.uv_coordinates.array) != self.num_nodes:
                 raise properties.ValidationError(
                     'texture[{index}] length {datalen} does not match '
                     'vertices length {meshlen}'.format(
                         index=i,
-                        datalen=len(tex.uv_coordinates),
+                        datalen=len(tex.uv_coordinates.array),
                         meshlen=self.num_nodes,
                     )
                 )
